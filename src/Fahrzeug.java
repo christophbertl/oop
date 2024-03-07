aaf66ed3410cc6b3d7059383e6547cb9a45116d1
christoph <christoph-bertl@t-online.de>
2024-03-07 19:53:42 +0100

    add file stream

--- src/Fahrzeug.java
+++ src/Fahrzeug.java
@@ -1,6 +1,13 @@
 
-public class Fahrzeug {
+// imports
+import java.io.Serializable;
 
+// implement Serializable to be compatible to In/ Output Streams in main class
+public class Fahrzeug implements Serializable {
+
+	// implement Serializable
+	private static final long serialVersionUID = 1L;
+	// create a attributes for vehicle
 	private String marke;
 	private String modell;
 	private String farbe;
@@ -10,11 +17,10 @@ public class Fahrzeug {
 	private String gemietetVon = null;
 	private String kennzeichen;
 	
-	
+	// constructor to create a car with all attributes
 	public Fahrzeug (String marke, String modell, String farbe, String kennzeichen,
 			int mietpreis, int sitzplatz, int kilometerstand)	{
-	
-	
+	// write given values to instance attributes
 	this.marke = marke;
 	this.modell = modell;
 	this.farbe = farbe;
@@ -22,95 +28,77 @@ public class Fahrzeug {
 	this.sitzplatz = sitzplatz;
 	this.kilometerstand = kilometerstand;
 	this.kennzeichen = kennzeichen;
-}
+	}
 
+	// getter and setter
+	public String getMarke () {
+		return marke;
+	}
 
-public String getMarke () {
+	public void setMarke (String marke) {
+		this.marke = marke;
+	}
+
+	public String getModell () {
+		return modell;
+	}
+
+	public void setModell (String modell) {
+		this.modell = modell;
+	}
+
+	public String getFarbe () {
+		return farbe;
+	}
+
+	public void setFarbe (String farbe) {
+		this.farbe = farbe;
+	}
+
+	public double getMietpreis () {
+		return mietpreis;
+	}
+
+	public void setMietpreis (double mietpreis) {
+		this.mietpreis = mietpreis;
+	}
+
+	public int getSitzplatz () {
+		return sitzplatz;
+	}
+
+	public void setSitzplatz (int sitzplatz) {
+		this.sitzplatz = sitzplatz;
+	}
+
+	public int getKilometerstand () {
+		return kilometerstand;
+	}
+
+	public void setKilometerstand (int kilometerstand) {
+		this.kilometerstand = kilometerstand;
+	}
+
+	public void setGemietetVon(String customerID) {
+		gemietetVon = customerID;
+	}
+
+	public String getGemietetVon() {
+		return gemietetVon;
+	}
 	
-	return marke;
-}
-
-public void setMarke (String marke) {
+	public String getKennzeichen () {
+		return kennzeichen;
+	}
 	
-	this.marke = marke;
-}
-
-public String getModell () {
-	
-	return modell;
-}
-
-public void setModell (String modell) {
-	
-	this.modell = modell;
-}
-
-public String getFarbe () {
-	
-	return farbe;
-}
-
-public void setFarbe (String farbe) {
-	
-	this.farbe = farbe;
-}
+	// methods
+	// display string of all information about this vehicle
+	public String getAll() {
+		return marke + " " + modell + "; " + farbe + "; " + sitzplatz + 
+				"; " + mietpreis + "; " + kilometerstand;
+	}
 
 
-public double getMietpreis () {
-	
-	return mietpreis;
-}
-
-public void setMietpreis (double mietpreis) {
-	
-	this.mietpreis = mietpreis;
-}
-
-
-
-
-public int getSitzplatz () {
-	
-	return sitzplatz;
-}
-
-public void setSitzplatz (int sitzplatz) {
-	
-	this.sitzplatz = sitzplatz;
-}
-
-
-
-public int getKilometerstand () {
-	
-	return kilometerstand;
-}
-
-public void setKilometerstand (int kilometerstand) {
-	
-	this.kilometerstand = kilometerstand;
-}
-
-public void setGemietetVon(String customerID) {
-	gemietetVon = customerID;
-
-}
-
-
-
-public String getGemietetVon() {
-	return gemietetVon;
-}
-
-public String getAll() {
-	return marke + " " + modell + "; " + farbe + "; " + sitzplatz + 
-			"; " + mietpreis + "; " + kilometerstand;
-}
-
-public String getKennzeichen () {
-	
-	return kennzeichen;
-}
 
 
 
